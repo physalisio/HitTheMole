d57f83ae379db7b70df33f8086a7106ed73064d4
Karin <Karin@karinsNotebook.fritz.box>
2015-07-03 23:18:45 +0200

    - single holes with background used - mole movement scaled

--- www/js/hitTheMole.js
+++ www/js/hitTheMole.js
@@ -1,60 +1,65 @@
-    window.onload = onDeviceReady;
+window.onload = onDeviceReady;
 
 var molePositions = [
-    {x: 212, y: 128},
-    {x: 490, y: 128},
-    {x: 80, y: 340},
-    {x: 350, y: 340},
-    {x: 620, y: 340}
+    {x: 314, y: 184},
+    {x: 710, y: 184},
+    {x: 124, y: 480},
+    {x: 510, y: 480},
+    {x: 890, y: 480}
 ];
-//var holePositions = [{x: , y: }];
+var holePositions = [
+    {x: 242, y: 184},
+    {x: 638, y: 184},
+    {x: 54, y: 482},
+    {x: 436, y: 482},
+    {x: 816, y: 482}];
 
 var points = 0;
 var aspectRatio;
 var enlargement = 1;
 var borderWidth = 0;
 var interval = 1000;
+var movementHeight = 10;
 
 function onDeviceReady() {
     setBackgroundSize();
 
-    initMoleClickHandler();
+    initMole();
+    initHoles();
     setTimeout("showTheMole()", interval);
 }
 
 function setBackgroundSize() {
-    var longestAspectRatio = 800 / 450;
+    var longestAspectRatio = 1136 / 640;
     var documentWidth = window.innerWidth;
     var documentHeight = window.innerHeight;
     aspectRatio = documentWidth / documentHeight;
     var background = document.getElementById("background");
-//    var holes = document.getElementById("holes");
-    
+
     if (aspectRatio >= longestAspectRatio) {
         background.style["height"] = documentHeight + "px";
-//        holes.style["height"] = documentHeight + "px";
         document.body.style["height"] = documentHeight + "px";
-//        background.style["width"] = documentWidth + "px";
-//        holes.style["width"] = documentWidth + "px";
         document.body.style["width"] = documentWidth + "px";
     } else {
-        var backgroundAspectRatio = 800 / 450;
+        var backgroundAspectRatio = 1136 / 640;
         var backgroundWidth = backgroundAspectRatio * documentHeight;
         background.style["height"] = documentHeight + "px";
-//        holes.style["height"] = documentHeight + "px";
         document.body.style["height"] = documentHeight + "px";
         background.style["width"] = backgroundWidth + "px";
-//        holes.style["width"] = backgroundWidth + "px";
         document.body.style["width"] = backgroundWidth + "px";
         borderWidth = (backgroundWidth - documentWidth) / 2;
         background.style["left"] = "-" + borderWidth + "px";
-//        holes.style["left"] = "-" + borderWidth + "px";
     }
 
-    enlargement = background.offsetHeight / 450;
+    enlargement = background.offsetHeight / 640;
+    movementHeight = movementHeight * enlargement;
+}
+
+function initMole() {
     var mole = document.getElementById("mole");
     mole.style["height"] = mole.offsetHeight * enlargement + "px";
     mole.style["width"] = mole.offsetWidth * enlargement + "px";
+    initMoleClickHandler();
 }
 
 function initMoleClickHandler() {
@@ -68,26 +73,52 @@ function initMoleClickHandler() {
     };
 }
 
+function initHoles() {
+    initHole("hole1", "holeBackground1", holePositions[0]);
+    initHole("hole2", "holeBackground2", holePositions[1]);
+    initHole("hole3", "holeBackground3", holePositions[2]);
+    initHole("hole4", "holeBackground4", holePositions[3]);
+    initHole("hole5", "holeBackground5", holePositions[4]);
+}
+
+function initHole(holeId, holeBackgroundId, holePosition) {
+    var hole = document.getElementById(holeId);
+    var height = hole.offsetHeight * enlargement + "px";
+    var width = hole.offsetWidth * enlargement + "px";
+    var positionX = holePosition["x"] * enlargement - borderWidth;
+    var positionY = holePosition["y"] * enlargement;
+    hole.style["height"] = height;
+    hole.style["width"] = width;
+    hole.style["left"] = positionX + "px";
+    hole.style["top"] = positionY + "px";
+
+    var holeBackground = document.getElementById(holeBackgroundId);
+    holeBackground.style["height"] = height;
+    holeBackground.style["width"] = width;
+    holeBackground.style["left"] = positionX + "px";
+    holeBackground.style["top"] = positionY + "px";
+}
+
 function showTheMole() {
     setMolePosition();
     initMoleClickHandler();
     var mole = document.getElementById("mole");
     mole.style.visibility = "visible";
-    
+
     var i = 6;
     var interval1 = setInterval(function () {
-        mole.style["top"] = (mole.offsetTop - 10) + "px";
-        
+        mole.style["top"] = (mole.offsetTop - movementHeight) + "px";
+
         if (i === 0) {
             setTimeout(function () {
                 var j = 0;
                 var interval2 = setInterval(function () {
-                    mole.style["top"] = (mole.offsetTop + 10) + "px";
-                    
+                    mole.style["top"] = (mole.offsetTop + movementHeight) + "px";
+
                     if (j === 6) {
                         clearInterval(interval2);
                         mole.style.visibility = "hidden";
-                        setTimeout(function(){
+                        setTimeout(function () {
                             showTheMole();
                         }, 3000);
                     }
